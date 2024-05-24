ed24bd8455c2df973734f71f2ff85c163483c2b2
c <ichlings@qq.com>
2024-05-24 22:23:34 +0800

    crypto

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,14 +1,18 @@
+#![allow(dead_code)]
+
 use aes::cipher::{block_padding::Pkcs7, BlockDecryptMut, BlockEncryptMut, KeyIvInit};
 
 type Aes128CbcEnc = cbc::Encryptor<aes::Aes128>;
 type Aes128CbcDec = cbc::Decryptor<aes::Aes128>;
 
+/// encrypt aes data
 fn encrypt_aes_cbc(key: &[u8], data: &[u8]) -> Vec<u8> {
     let iv = &[0u8; 16];
     let res = Aes128CbcEnc::new(key.into(), iv.into()).encrypt_padded_vec_mut::<Pkcs7>(data);
     res.to_vec()
 }
 
+/// decrypt aes data
 fn decrypt_aes_cbc(key: &[u8], data: &[u8]) -> Vec<u8> {
     let iv = &[0u8; 16];
     let res = Aes128CbcDec::new(key.into(), iv.into())
